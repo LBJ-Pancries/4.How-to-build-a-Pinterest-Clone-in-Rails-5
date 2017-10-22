2e97b07f4890fd616cea7cc2d7c64e1fe28b8c9c
liubingjie <breeder1121@hotmail.com>
2017-10-22 18:25:14 +0800

    install devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :uesrs
   resources :pins
   root "pins#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
